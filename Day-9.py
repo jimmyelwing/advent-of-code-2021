014c316b66652677c2603e85907b93236327a285
Jimmy Elwing <accounts@jimmyelwing.se>
2021-12-09 22:23:06 +0100

    Refactored some more, much cleaner now.

--- Day-9.py
+++ Day-9.py
@@ -1,60 +1,49 @@
 def main():
-    # inputs = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678"
-    # inputs = inputs.split("\n")
+    inputs = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678"
+    inputs = inputs.split("\n")
 
     inputs = open("inputs/day-9.txt", "r").read().split("\n")
 
-    # this is a list of numbers, new line for each row of numbers
+    print(PartOne(inputs))
 
-    positions = {}
-    county = 0
+def PartOne(inputs):
 
-    maxLengthOfRow = len(inputs[0]) - 1
-    numberOfRows = 0
-    for row in inputs:
-        numberOfRows += 1
-        countx = 0
-
-        for number in row:
-            x = countx
-            y = county
-            positions[x, y] = number
-            countx += 1
-
-        county += 1
+    positions = GetPositionsFromInputs(inputs)
+    numberOfColumns, numberOfRows = max(positions)
 
     adjacentNumbers = []
     totalSum = 0
 
-    for i in positions:
-        number = int(positions[i])
-        xposition = i[0]
-        yposition = i[1]
-        right = xposition + 1, yposition
-        left = xposition - 1, yposition
-        down = xposition, yposition + 1
-        up = xposition, yposition - 1
+    for position in positions:
+        number = int(positions[position])
+        x,y = position
+    
+        right = x + 1, y
+        left = x - 1, y
+        down = x, y + 1
+        up = x, y - 1
 
-        if xposition == 0:
-            if yposition == 0:
+        if FirstColumn(x):
+            if FirstRow(y):
                 adjacentNumbers = GetNumbers(positions, (down, right))
-            if yposition > 0 and yposition < numberOfRows - 1:
+            elif MiddleRow(y, numberOfRows):
                 adjacentNumbers = GetNumbers(positions, (down, right, up))
-            if yposition == numberOfRows - 1:
+            elif LastRow(y, numberOfRows):
                 adjacentNumbers = GetNumbers(positions, (right, up))
-        elif xposition > 0 and xposition < maxLengthOfRow:
-            if yposition == 0:
+        elif MiddleColumn(x, numberOfColumns):
+            if FirstRow(y):
                 adjacentNumbers = GetNumbers(positions, (down, right, left))
-            elif yposition > 0 and yposition < numberOfRows - 1:
-                adjacentNumbers = GetNumbers(positions, (up, right, left, down))
-            elif xposition < maxLengthOfRow and yposition == numberOfRows - 1:
+            elif MiddleRow(y, numberOfRows):
+                adjacentNumbers = GetNumbers(
+                    positions, (up, right, left, down))
+            elif LastRow(y, numberOfRows):
                 adjacentNumbers = GetNumbers(positions, (right, left, up))
-        elif xposition == maxLengthOfRow:
-            if yposition == 0:
-                adjacentNumbers = GetNumbers(positions, (down,left))
-            if yposition > 0 and yposition < numberOfRows - 1:
-                adjacentNumbers = GetNumbers(positions, (down,left,up))
-            elif yposition == numberOfRows - 1:
+        elif LastColumn(x, numberOfColumns):
+            if FirstRow(y):
+                adjacentNumbers = GetNumbers(positions, (down, left))
+            elif MiddleRow(y, numberOfRows):
+                adjacentNumbers = GetNumbers(positions, (down, left, up))
+            elif LastRow(y, numberOfRows):
                 adjacentNumbers = GetNumbers(positions, (left, up))
 
         if any(adjacentNumbers):
@@ -68,13 +57,53 @@ def main():
             if smaller:
                 totalSum += number + 1
 
-    print(totalSum)
+    return totalSum
+
+def GetPositionsFromInputs(inputs):
+    positions = {}
+    county = 0
+    for row in inputs:
+        countx = 0
+        for number in row:
+            x = countx
+            y = county
+            positions[x, y] = number
+            countx += 1
+        county += 1
+        
+    return positions
+
 
 def GetNumbers(positions, directions):
     adjacentNumbers = []
     for i in directions:
         adjacentNumbers.append(int(positions[i]))
-    return adjacentNumbers 
+    return adjacentNumbers
+
+
+def FirstRow(y):
+    return y == 0
+
+
+def MiddleRow(y, numberOfRows):
+    return y > 0 and y < numberOfRows
+
+
+def LastRow(y, numberOfRows):
+    return y == numberOfRows
+
+
+def FirstColumn(x):
+    return x == 0
+
+
+def MiddleColumn(x, numberOfColumns):
+    return x > 0 and x < numberOfColumns
+
+
+def LastColumn(x, numberOfColumns):
+    return x == numberOfColumns
+
 
 if __name__ == "__main__":
     main()
